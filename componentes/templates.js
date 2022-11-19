2b2298b799bedf2feab6644635ce40b25edc1e10
Dev.AugustoMelo <guga7msn@hotmail.com>
2022-11-19 10:44:04 -0300

    Criação da página sobre

--- componentes/templates.js
+++ componentes/templates.js
@@ -31,22 +31,22 @@ class MenuLateral extends HTMLElement {
   <nav id="nav-bar" class="nav-menu">
     <ul class="nav flex-column" >
       <li class="nav-item" >
-        <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house" aria-hidden="true"></i> Home</a>
+        <a class="nav-link active" aria-current="page" href="#home"><i class="bi bi-house" aria-hidden="true"></i> Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link active" href="/index.html" target="_blank"><i class="bi bi-person" aria-hidden="true"></i> Sobre</a>
+        <a class="nav-link active" href="#sobre" target="_blank"><i class="bi bi-person" aria-hidden="true"></i> Sobre</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link active" href="#"><i class="bi bi-list-check" aria-hidden="true"></i> Habilidades</a>
+        <a class="nav-link active" href="#formacao"><i class="bi bi-list-check" aria-hidden="true"></i> Habilidades</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link active" href="#"><i class="bi bi-file-earmark" aria-hidden="true"></i> Currículo</a>
+        <a class="nav-link active" href="#curriculo"><i class="bi bi-file-earmark" aria-hidden="true"></i> Currículo</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link active" href="#"><i class="bi bi-collection" aria-hidden="true"></i> Portfólio</a>
+        <a class="nav-link active" href="#portfolio"><i class="bi bi-collection" aria-hidden="true"></i> Portfólio</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link active" href="#"><i class="bi bi-envelope" aria-hidden="true"></i> Contato</a>
+        <a class="nav-link active" href="#contato"><i class="bi bi-envelope" aria-hidden="true"></i> Contato</a>
       </li>
     </ul>
   </nav>

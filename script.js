7e167216704743c443d6d8d4b6077f7467f91a22
Dev.AugustoMelo <guga7msn@hotmail.com>
2022-11-22 22:02:04 -0300

    Alternar exibição do menu no modo mobile

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 
 
-/** Avtivar menu lateral em modo mobile */
+/** Ativar menu lateral em modo mobile */
 
 
 const menuMobile = document.querySelector('.menu-mobile')
@@ -14,4 +14,19 @@ menuMobile.addEventListener('click', () => {
     ? menuMobile.classList.replace("bi-list", "bi-x"): menuMobile.classList.replace("bi-x", "bi-list")
 
     body.classList.toggle("nav-menu-active")
+})
+
+
+// Alternar exibição do menu
+
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item =>{
+    item.addEventListener('click', () => {
+        if(body.classList.contains("nav-menu-active")){
+            body.classList.remove("nav-menu-active")
+            menuMobile.classList.replace("bi-x", "bi-list" )
+        }
+    })
 })

a4e56b1a395861151d68f5a95d0a19a0806ca1be
Augusto Melo <guga7msn@hotmail.com>
2022-12-09 02:58:59 -0300

    Seção mensagem dos contatos e localStorage

--- script.js
+++ script.js
@@ -64,3 +64,69 @@ function active(tag){
         const texto = document.form.mensagem.value;
         document.getElementById('texto').innerText = texto.length; // texto.split(/\s+/).length  para contar palavras ou (/\n/) para linhas.
       }
+
+
+      // Receber seletor contato
+
+      const forms = document.getElementById('contato-forms');
+
+      // cria um array para objetos json 
+      let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+
+      // listar mensagens ao carregar a página
+      usuarios.forEach((element)=>{
+        document.getElementById("listar-mensagem").insertAdjacentHTML('beforeend', "Nome: "+ element.nome + "<br/>"+ "Email: " + element.email + "<br/><br/>" +  element.mensagem + "<hr/> <br/>" )
+      })
+
+      // adicionar no localStorage ao enviar formulário
+      forms.addEventListener("submit", (e) => {
+        console.log("cliquei")
+        
+        
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const msg = document.getElementById('mensagem').value;
+
+        //não recarregar a página
+        e.preventDefault();
+        // console.log(nome)
+        // console.log(email)
+        // console.log(msg)
+
+        
+
+        // adicionando objetos dentro do array
+        usuarios.push({nome, email, mensagem:msg})
+
+        // muda a estrutura da variável para objetos json string
+        localStorage.setItem("usuarios", JSON.stringify(usuarios))
+
+        // lista no final as mensagens enviadas
+        document.getElementById("listar-mensagem").insertAdjacentHTML('beforeend', "Nome: "+ nome + "<br/>"+ "Email: " + email + "<br/><br/>" +  msg + "<hr/> <br/>" )
+
+      })
+
+
+
+
+
+      // Mostrar ou Esconder section de Mensagens do contato
+
+      var btn = document.querySelector("#mostra-esconde")
+      var section = document.querySelector(".mensagem-div")
+
+      btn.addEventListener('click', () => {
+        section.style.display != "none" ? section.style.display = "none": section.style.display = "block"
+
+      })
+
+
+      // Excluir mensagens
+
+      var excluir = document.querySelector('#excluir-mensagens')
+
+      excluir.addEventListener('click', (excluir) =>{
+        localStorage.clear()
+        
+      })
